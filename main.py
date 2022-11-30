357a5db32d2f911aa677f4ef08b00037ef9b898f
oqueli-training <108534846+oqueli-training@users.noreply.github.com>
2022-11-30 11:58:32 -0600

    Crated Main.py
    Cloud function code to deploy on the scoping project

---
+++ main.py
@@ -0,0 +1,26 @@
+from google.cloud import logging
+import random
+
+def hello_world (request):
+
+	logging_client = logging.Client()
+
+	logger = logging_client.logger("hiramlogs")
+
+	logger.log_struct(
+	{"data": {"ratesPerSecond": {"price": random.randint(0,5)}}}
+	)
+
+	print("Wrote logs to {}.".format(logger.name))
+
+
+
+	logger2 = logging_client.logger("hiramlogs2")
+
+	logger2.log_struct(
+	{"data": {"market": {"open": 0 }}}
+	)
+
+	print("Wrote logs to {}.".format(logger2.name))
+
+	return f"hecho!"
